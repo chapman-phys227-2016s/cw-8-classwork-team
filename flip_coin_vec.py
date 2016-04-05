82753f3dea524ed3c6d869837e8d8c7bc2e6af7b
Austin Ayers <ayers111@mail.chapman.edu>
2016-04-05 17:37:02 +0000

    Optimization

--- flip_coin_vec.py
+++ flip_coin_vec.py
@@ -4,7 +4,4 @@ def flip(N):
     """
     Simulates tossing a coin N times, and writes out the number of tails.
     """
-    answer = np.random.randint(2, size=N)
-    tails = np.where(answer > 0, 0, 1)
-    return np.sum(tails)
-print flip(100)
+    return np.random.randint(2, size=N).sum()
