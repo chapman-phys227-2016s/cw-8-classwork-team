7614d61cf88b13fdc0b75ea397fa19b2c49dad58
Taylor Patti <patti102@mail.chapman.edu>
2016-05-03 17:51:43 +0000

    Fixed files.

--- walk2D_class.py
+++ walk2D_class.py
@@ -15,10 +15,8 @@ from Particles import Particles
 from Particle import Particle
 import sys
 
-def main():
-
-    if __name__ == "__main__": main():
-        partics = Particles([ Particle() for i in xrange(int(sys.argv[1]))])
-        partics.moves(int(sys.argv[2]))
+if __name__ == "__main__":
+    partics = Particles([ Particle() for i in xrange(int(sys.argv[1]))])
+    partics.moves(int(sys.argv[2]))
 
 
