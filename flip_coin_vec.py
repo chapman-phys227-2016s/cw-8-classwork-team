e7ef2d18c77971b4d540b28233f70cd931ade2e6
Taylor Patti <patti102@mail.chapman.edu>
2016-05-03 17:14:46 +0000

    Fixed the errors in this file.

--- flip_coin_vec.py
+++ flip_coin_vec.py
@@ -4,4 +4,6 @@ def flip(N):
     """
     Simulates tossing a coin N times, and writes out the number of tails.
     """
-    return np.random.randint(2, size=N).sum()
+    return np.random.randint(0, high=2, size=int(N)).sum()
+
+print flip(10)
